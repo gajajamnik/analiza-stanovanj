843db015e801c7132cc5f01aaa3e06a0be5707ee
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-11-02 12:42:24 +0100

    dodan zajem posameznh oglasov

--- zajem.py
+++ zajem.py
@@ -1,18 +1,34 @@
 import re
 import orodja
 
-url = 'https://www.nepremicnine.net/oglasi-oddaja/stanovanje/'
+# zajem podatkov iz strani vsake nepremicnine posebej
 
-mapa = 'zajeti_podatki'
-
+def zajem_posameznega_oglasa(seznam):
+    for url, id in seznam:
+        datoteka = f'zajete_strani/oglasi/{id}.html'
+        orodja.shrani_spletno_stran(url, datoteka)
+        print(f'Uspesno shranjen oglas id: {id}')
 
 # zajem podatkov iz glavnih strani
-def stanovanja_na_strani(st_strani=57):
+def zajem_strani(st_strani=57):
+    
+    seznam = []
+    vzorec_url_id = r'<!--<meta itemprop="url" content="(.+stanovanje_(.+)/)" />--'
+    
     for i in range(st_strani):
-        url = f'https://www.nepremicnine.net/oglasi-oddaja/stanovanje/{i}/'
+        url_strani = f'https://www.nepremicnine.net/oglasi-oddaja/stanovanje/{i}/'
         datoteka = f'zajete_strani/stanovanja/stanovanja{i * 30 + 1}-{(i+1) * 30}.html'
-        orodja.shrani_spletno_stran(url, datoteka)
+        orodja.shrani_spletno_stran(url_strani, datoteka)
+        print(f'Uspesno sharnjena stran{i}')
+
+        #ustvari seznam naborov (url, id)
+        seznam_url_id = re.findall(vzorec_url_id, orodja.vsebina_datoteke(datoteka))
+        seznam += seznam_url_id
+        print(f'Dolzina seznama: {len(seznam)}')
+    
+    #zajamemo še vsako stran posebaj
+    zajem_posameznega_oglasa(seznam)
 
 
-stanovanja_na_strani()
-# zajem podatkov iz strani vsake nepremicnine posebej
+zajem_strani()
+
